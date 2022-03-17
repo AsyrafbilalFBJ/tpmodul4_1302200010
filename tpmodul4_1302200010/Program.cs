21b88c9454e5a0b719c73264457f8178520f365c
AsyrafbilalFBJ <asyrafbilalrpltelu@student.telkomuniversity.ac.id>
2022-03-17 16:45:19 +0700

    menambahkan class DataGeneric

--- tpmodul4_1302200010/Program.cs
+++ tpmodul4_1302200010/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace tpmodul4_1302200010
 {
+    class HaloGeneric
+    {
+        public static void SapaUser<T>(T X)
+        {
+            Console.WriteLine("Halo user " + X);
+        }
+    }
+    class DataGeneric
+    {
+        static void dataGeneric<T>(T data)
+        {
+            Console.Write(data);
+        }
+        public static void printData()
+        {
+            Console.Write("Data yang tersimpan adalah: ");
+            dataGeneric<int>(1302200010);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            HaloGeneric.SapaUser<String>("Asyraf");
+            DataGeneric.printData();
+            Console.ReadKey();
         }
     }
 }
